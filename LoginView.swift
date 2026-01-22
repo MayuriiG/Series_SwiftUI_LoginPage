28f7c407372ccaaa320e5c0e071aab8686898876
MayuriiG <59797586+MayuriiG@users.noreply.github.com>
2026-01-22 21:51:53 +0800

    Create LoginView.swift
    Lets addAPI call to LOGIN Screen

---
+++ LoginView.swift
@@ -0,0 +1,45 @@
+//
+//  LoginView.swift
+//  Github_LoginPAGE_Swiftui
+//
+//  Created by user252002 on 1/22/26.
+//
+//takingdata from ViewModel-----------------------ViewMODEL API ||
+import SwiftUI
+
+
+struct LoginView: View {
+    
+    @StateObject private var viewModel = LoginModelView()
+    
+    var body: some View {
+        VStack{
+            Text("Login").font(.largeTitle)
+            TextField("Email",text: $viewModel.email).textFieldStyle(RoundedBorderTextFieldStyle())
+            SecureField("Password",text: $viewModel.pass).textFieldStyle(RoundedBorderTextFieldStyle())
+            
+           
+            Button("Login"){
+                if !viewModel.email.isEmpty && !viewModel.pass.isEmpty{
+                    
+                    Task{
+                        print("Button")
+                        await viewModel.loginCall()
+                    }
+                }
+            }
+            .frame(maxWidth: .infinity)
+            .padding().background(Color.blue.opacity(0.60))
+            .foregroundStyle(.white).cornerRadius(10)
+            .allowsHitTesting(true)
+            
+            Text(viewModel.mess).foregroundColor(.green).padding()
+          /*  Text(viewModel.token).font(.largeTitle).multilineTextAlignment(.center)*/
+            
+        }.padding()
+    }
+}
+
+#Preview {
+    LoginView()
+}
